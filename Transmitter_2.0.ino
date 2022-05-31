4b74380b23beccba097495e6ad84f36b3a001ba1
SPChip <taskomport@mail.ru>
2022-05-31 13:06:44 +0300

    2

--- Transmitter_2.0.ino
+++ Transmitter_2.0.ino
@@ -1,6 +1,8 @@
 //SPChip Transmitter 2.0
 
 //--------------------- НАСТРОЙКИ ----------------------
+#define INIT_ADDR 1023        // номер резервной ячейки для инициализации первого запуска
+#define INIT_KEY 33           // ключ первого запуска. 0-254, на выбор, надо поменять на любое значение и будет как впервый раз
 #define KEY1_PIN 11            // кнопка KEY1 подключена сюда 
 #define KEY2_PIN 5
 #define KEY3_PIN 13
@@ -60,12 +62,12 @@
 #define SW4_1_BIT 2
 #define SW4_2_BYTE lo_byte
 #define SW4_2_BIT 1
-
 //--------------------- НАСТРОЙКИ ----------------------
 
 //--------------------- БИБЛИОТЕКИ ----------------------
 //#include <SPI.h>
 #include <Wire.h>
+#include <EEPROM.h>
 #include "nRF24L01.h"
 #include "RF24.h"
 #include "GyverButton.h"      // Библиотека для кнопок
@@ -84,14 +86,32 @@ GTimer LCD_TIMER(MS, 100);                        // создаем таймер
 //--------------------- ОБЪЕКТЫ ----------------------
 
 //--------------------- ПЕРЕМЕННЫЕ ----------------------
-uint16_t t = 0;
-uint8_t hi_byte, lo_byte;
-
+uint16_t read_data [18];    // массив опроса кнопок и крутилок
 uint8_t address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"}; // возможные номера труб
 uint16_t transmit_data[7];     // массив пересылаемых данных
 uint16_t telemetry[2];          // массив принятых от приёмника данных телеметрии
 uint8_t rssi;                 //
 uint16_t trnsmtd_pack = 1, failed_pack; // переданные и потерянные пакеты
+uint8_t set_default[18][3] = {          // настройка каналов по умолчанию
+      {0, 0, 255},     //CH1 - J1X
+      {1, 0, 255},     //CH2 - J1Y
+      {2, 0, 255},     //CH3 - J2X
+      {3, 0, 255},     //CH4 - J2Y
+      {4, 0, 255},     //CH5 - RP1
+      {5, 0, 255},     //CH5 - RP2
+      {6, 0, 1},
+      {7, 0, 1},
+      {8, 0, 1},
+      {9, 0, 1},
+      {10, 0, 1},
+      {11, 0, 1},
+      {12, 0, 1},
+      {13, 0, 1},
+      {14, 0, 1},
+      {15, 0, 1},
+      {16, 0, 2},   
+      {17, 0, 2},         
+    };
 //--------------------- ПЕРЕМЕННЫЕ ----------------------
 
 
@@ -104,13 +124,28 @@ void setup() {
   LCD.initR(INITR_144GREENTAB); // Init ST7735R chip, green tab
   LCD.fillScreen(ST77XX_BLACK);
   Wire.begin();
-  Wire.requestFrom(0x27, 2);
-  lo_byte = Wire.read(); // Читаем младший байт (P00...P07)
-  hi_byte = Wire.read(); // Читаем старший байт (P10...P17)
   RadioSetup();
+  if (EEPROM.read(INIT_ADDR) != INIT_KEY) { // первый запуск
+    EEPROM.write(INIT_ADDR, INIT_KEY);      // записали ключ    
+    EEPROM.put(100, set_default);       // режим экрана 1
+  }
 }
 
 void loop() {
+  KEY1.tick();                                   // постоянно проверяем первую кнопку
+  KEY2.tick();
+  KEY3.tick();
+  ReadData();                                    // опрашиваем все кнопки и крутилки
+  if (KEY1.isClick()) digitalWrite(PinPower_PIN, LOW);    // проверка на один клик
+  if (LCD_TIMER.isReady()) {
+    Display_TestKey();
+  }
+  for (uint8_t i = 0; i < 18; i++) {
+    Serial.print(read_data [i]);
+    Serial.print("  ");
+  }
+  Serial.println("  ");
+
   transmit_data[0] = 0x55;
   transmit_data[1] = map(analogRead(J1Y_pin), 0, 1023, 0, 255);         // считываем показания потенциометра с вывода A1 и записываем их в 2 элемент массива data
   transmit_data[4] = analogRead(J2Y_pin);
@@ -130,63 +165,41 @@ void loop() {
   }
 
 
-  KEY1.tick();                                   // постоянно проверяем первую кнопку
-  KEY2.tick();
-  KEY3.tick();
-  if (KEY1.isClick()) digitalWrite(PinPower_PIN, LOW);    // проверка на один клик
-  if (LCD_TIMER.isReady()) {
 
-    Display_TestKey();
-  }
-  Wire.requestFrom(0x27, 2);
-  lo_byte = Wire.read(); // Читаем младший байт (P00...P07)
-  hi_byte = Wire.read(); // Читаем старший байт (P10...P17)
-  if (bitRead(hi_byte, 0)) Serial.print(1);
-  if (!bitRead(hi_byte, 0)) Serial.print(0);
-  if (bitRead(hi_byte, 1)) Serial.print(1);
-  if (!bitRead(hi_byte, 1)) Serial.print(0);
-  if (bitRead(hi_byte, 2)) Serial.print(1);
-  if (!bitRead(hi_byte, 2)) Serial.print(0);
-  if (bitRead(hi_byte, 3)) Serial.print(1);
-  if (!bitRead(hi_byte, 3)) Serial.print(0);
-  if (bitRead(hi_byte, 4)) Serial.print(1);
-  if (!bitRead(hi_byte, 4)) Serial.print(0);
-  if (bitRead(hi_byte, 5)) Serial.print(1);
-  if (!bitRead(hi_byte, 5)) Serial.print(0);
-  if (bitRead(hi_byte, 6)) Serial.print(1);
-  if (!bitRead(hi_byte, 6)) Serial.print(0);
-  if (bitRead(hi_byte, 7)) Serial.print(1);
-  if (!bitRead(hi_byte, 7)) Serial.print(0);
-  if (bitRead(lo_byte, 0)) Serial.print(1);
-  if (!bitRead(lo_byte, 0)) Serial.print(0);
-  if (bitRead(lo_byte, 1)) Serial.print(1);
-  if (!bitRead(lo_byte, 1)) Serial.print(0);
-  if (bitRead(lo_byte, 2)) Serial.print(1);
-  if (!bitRead(lo_byte, 2)) Serial.print(0);
-  if (bitRead(lo_byte, 3)) Serial.print(1);
-  if (!bitRead(lo_byte, 3)) Serial.print(0);
-  if (bitRead(lo_byte, 4)) Serial.print(1);
-  if (!bitRead(lo_byte, 4)) Serial.print(0);
-  if (bitRead(lo_byte, 5)) Serial.print(1);
-  if (!bitRead(lo_byte, 5)) Serial.print(0);
-  if (bitRead(lo_byte, 6)) Serial.print(1);
-  if (!bitRead(lo_byte, 6)) Serial.print(0);
-  if (bitRead(lo_byte, 7)) Serial.print(1);
-  if (!bitRead(lo_byte, 7)) Serial.print(0);
-  Serial.print("  ");
-  Serial.print(analogRead(J1X_pin));
-  Serial.print("  ");
-  Serial.print(analogRead(J1Y_pin));
-  Serial.print("  ");
-  Serial.print(analogRead(J2X_pin));
-  Serial.print("  ");
-  Serial.print(analogRead(J2Y_pin));
-  Serial.print("  ");
-  Serial.print(String(transmit_data[0]) + "    " + String(transmit_data[1]) + "    " + String(transmit_data[2]) + "    " + String(transmit_data[3]) + "    " + String(transmit_data[4]) + "    " + String(transmit_data[5]) + "    " + String(transmit_data[6]));
-  Serial.print("    ");
-  Serial.println(telemetry[0] * 0.00488);
+
+
+  //  Serial.print(String(transmit_data[0]) + "    " + String(transmit_data[1]) + "    " + String(transmit_data[2]) + "    " + String(transmit_data[3]) + "    " + String(transmit_data[4]) + "    " + String(transmit_data[5]) + "    " + String(transmit_data[6]));
+  //  Serial.print("    ");
+  //  Serial.println(telemetry[0] * 0.00488);
 }
 
+void ReadData() {
+  read_data [0] = analogRead(J1X_pin); // J1X
+  read_data [1] = analogRead(J1Y_pin); // J1Y
+  read_data [2] = analogRead(J2X_pin); // J2X
+  read_data [3] = analogRead(J2Y_pin); // J2Y
+  read_data [4] = analogRead(RP1_pin); // RP1
+  read_data [5] = analogRead(RP2_pin); // RP2
+  Wire.requestFrom(0x27, 2); // отправляем запрос в расширитель портов
+  uint8_t lo_byte = Wire.read(); // Читаем младший байт (P00...P07)
+  uint8_t hi_byte = Wire.read(); // Читаем старший байт (P10...P17)
+  read_data [6] = !bitRead(LEFT_BYTE, LEFT_BIT);      // LEFT
+  read_data [7] = !bitRead(UP_BYTE, UP_BIT);          // UP
+  read_data [8] = !bitRead(DOWN_BYTE, DOWN_BIT);      // DOWN
+  read_data [9] = !bitRead(RIGHT_BYTE, RIGHT_BIT);    // RIGHT
+  read_data [10] = !bitRead(YEL_BYTE, YEL_BIT);       // YEL
+  read_data [11] = !bitRead(WHITE_BYTE, WHITE_BIT);   // WHITE
+  read_data [12] = !bitRead(BLUE_BYTE, BLUE_BIT);     // BLUE
+  read_data [13] = !bitRead(RED_BYTE, RED_BIT);       // RED
+  read_data [14] = !bitRead(J1KEY_BYTE, J1KEY_BIT);   // J1KEY
+  read_data [15] = !bitRead(J2KEY_BYTE, J2KEY_BIT);   // J2KEY
+  if (!bitRead(SW3_1_BYTE, SW3_1_BIT))  read_data [16] = 0;                                       // SW3
+  if (bitRead(SW3_1_BYTE, SW3_1_BIT) && bitRead(SW3_2_BYTE, SW3_2_BIT)) read_data [16] = 1;
+  if (!bitRead(SW3_2_BYTE, SW3_2_BIT)) read_data [16] = 2;
+  if (!bitRead(SW4_1_BYTE, SW4_1_BIT))  read_data [17] = 0;                                       // SW4
+  if (bitRead(SW4_1_BYTE, SW4_1_BIT) && bitRead(SW4_2_BYTE, SW4_2_BIT)) read_data [17] = 1;
+  if (!bitRead(SW4_2_BYTE, SW4_2_BIT)) read_data [17] = 2;
+}
 
 
 void RadioSetup() {
